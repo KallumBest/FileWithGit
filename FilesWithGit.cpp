b48dffd536790fa394222799e7b9428b3249a7d8
Kallum Best <kallum.p.best@gmail.com>
2021-06-08 20:43:46 +1200

    first commit

---
+++ FilesWithGit.cpp
@@ -0,0 +1,21 @@
+// FilesWithGit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>  
+#include <string>
+using namespace std;
+
+struct Shoes
+{
+	
+	string brand;
+	string material;
+	string color;
+	int size;
+	int cost;
+};
+int main(void) {
+	struct Shoes s;
+	
+	return 0;
+}
